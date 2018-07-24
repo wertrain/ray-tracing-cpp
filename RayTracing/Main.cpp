8fc7fd3e0a68943115f39c8732566b56ba075465
wertrain <wertrain@gmail.com>
2018-07-25 01:20:12 +0900

    Vector クラスの追加

--- RayTracing/Main.cpp
+++ RayTracing/Main.cpp
@@ -1,6 +1,14 @@
 #include <cinttypes>
 #include "PPM.h"
 
+struct V
+{
+    double x, y, z;
+    V(const double v) : x(v), y(v), z(v) {}
+    V(const double x, const double y, const double z) : x(x), y(y), z(z) {}
+    double &operator[](int i) const { (&x)[i]; }
+};
+
 int main()
 {
     PPM ppm(400, 320);
