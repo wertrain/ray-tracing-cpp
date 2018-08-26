bfddc40085b2b9aab4c351f01bd35cacc487fde9
wertrain <wertrain@gmail.com>
2018-08-26 15:34:14 +0900

    反射する材質の設定に対応

--- RayTracing/Main.cpp
+++ RayTracing/Main.cpp
@@ -36,13 +36,21 @@ namespace
         V n;                  ///< p での法線
     };
 
+    /// 材質のタイプ
+    enum class SurfaceType
+    {
+        Diffuse, ///< 拡散面
+        Mirror,  ///< 鏡面
+    };
+
     /// 球
     struct Sphere
     {
-        V p;       ///< 中心位置
-        double r;  ///< 半径
-        V R;       ///< 反射率 [Reflectance]
-        V Le;      ///< 照度 [Illuminance]
+        V p;              ///< 中心位置
+        double r;         ///< 半径
+        SurfaceType type; ///< 表面材質の種類
+        V R;              ///< 反射率 [Reflectance]
+        V Le;             ///< 照度 [Illuminance]
 
         std::optional<Hit> Intersect(const Ray& ray, const double tmin, const double tmax) const
         {
@@ -150,21 +158,18 @@ struct RenderParam
 void chapter1(std::vector<V>& image, const RenderParam& param)
 {
     Scene scene;
-    //scene.spheres.push_back({ V(0), 1 });
-
-    scene.spheres.push_back({ V(27,16.5,47), 16.5, V(.999) });
-    scene.spheres.push_back({ V(73,16.5,78), 16.5, V(.999) });
-
+    // Spheres
+    scene.spheres.push_back({ V(27,16.5,47), 16.5, SurfaceType::Diffuse, (.999) });
+    scene.spheres.push_back({ V(73,16.5,78), 16.5, SurfaceType::Diffuse, V(.999) });
     // Cornell Box
-    scene.spheres.push_back({ V(1e5 + 1, 40.8, 81.6),   1e5, V(.75, .25, .25) });  // Left 
-    scene.spheres.push_back({ V(-1e5 + 99, 40.8, 81.6), 1e5, V(.25, .25, .75) });  // Right 
-    scene.spheres.push_back({ V(50, 40.8, 1e5),         1e5, V(.75, .75, .75) });  // Back
-                                                                                   //scene.spheres.push_back({ V(50, 40.8, -1e5 + 170), 1e5, V(.75, .75, .75) }); // Front
-    scene.spheres.push_back({ V(50, 1e5, 81.6),         1e5, V(.75, .75, .75) });  // Bottom
-    scene.spheres.push_back({ V(50, -1e5 + 81.6, 81.6), 1e5, V(.75, .75, .75) });  // Top
-
-                                                                                   // Light
-    scene.spheres.push_back({ V(50,681.6 - .27,81.6), 600, V(), V(12) });
+    scene.spheres.push_back({ V(1e5 + 1, 40.8, 81.6),   1e5, SurfaceType::Diffuse, V(.75, .25, .25) });  // Left 
+    scene.spheres.push_back({ V(-1e5 + 99, 40.8, 81.6), 1e5, SurfaceType::Diffuse, V(.25, .25, .75) });  // Right 
+    scene.spheres.push_back({ V(50, 40.8, 1e5),         1e5, SurfaceType::Diffuse, V(.75, .75, .75) });  // Back
+    //scene.spheres.push_back({ V(50, 40.8, -1e5 + 170), 1e5, SurfaceType::Diffuse, V(.75, .75, .75) }); // Front
+    scene.spheres.push_back({ V(50, 1e5, 81.6),         1e5, SurfaceType::Diffuse, V(.75, .75, .75) });  // Bottom
+    scene.spheres.push_back({ V(50, -1e5 + 81.6, 81.6), 1e5, SurfaceType::Diffuse, V(.75, .75, .75) });  // Top
+    // Light
+    scene.spheres.push_back({ V(50,681.6 - .27,81.6), 600, SurfaceType::Diffuse, V(), V(12) });
 
     const int width = param.width;
     const int height = param.height;
@@ -261,11 +266,137 @@ void chapter1(std::vector<V>& image, const RenderParam& param)
     std::cout << elapsed << " millisec." << std::endl;
 }
 
+void chapter2(std::vector<V>& image, const RenderParam& param)
+{
+    Scene scene;
+    scene.spheres.push_back({ V(27,16.5,47), 16.5, SurfaceType::Mirror, (.999) });
+    scene.spheres.push_back({ V(73,16.5,78), 16.5, SurfaceType::Mirror, V(.999) });
+    // Cornell Box
+    scene.spheres.push_back({ V(1e5 + 1, 40.8, 81.6),   1e5, SurfaceType::Diffuse, V(.75, .25, .25) });  // Left 
+    scene.spheres.push_back({ V(-1e5 + 99, 40.8, 81.6), 1e5, SurfaceType::Diffuse, V(.25, .25, .75) });  // Right 
+    scene.spheres.push_back({ V(50, 40.8, 1e5),         1e5, SurfaceType::Diffuse, V(.75, .75, .75) });  // Back
+    //scene.spheres.push_back({ V(50, 40.8, -1e5 + 170), 1e5, SurfaceType::Diffuse, V(.75, .75, .75) }); // Front
+    scene.spheres.push_back({ V(50, 1e5, 81.6),         1e5, SurfaceType::Diffuse, V(.75, .75, .75) });  // Bottom
+    scene.spheres.push_back({ V(50, -1e5 + 81.6, 81.6), 1e5, SurfaceType::Diffuse, V(.75, .75, .75) });  // Top
+    // Light
+    scene.spheres.push_back({ V(50,681.6 - .27,81.6), 600, SurfaceType::Diffuse, V(), V(12) });
+
+    const int width = param.width;
+    const int height = param.height;
+    const int SamplesPerPixel = param.samplesPerPixel;
+
+    // カメラパラメータ設定
+    Camera camera;
+    camera.eye = V(50.0, 52.0, 295.6);
+    camera.center = camera.eye + V(0.0, -0.042612, -1.0);
+    camera.up = V(0.0, 1.0, 0.0);
+    camera.fov = DegreeToRadian(30.0);
+    camera.aspect = static_cast<double>(width) / static_cast<double>(height);
+
+    V wE, uE, vE;
+    camera.Calculate(wE, uE, vE);
+
+    // 時間計測の開始
+    const std::chrono::system_clock::time_point start = std::chrono::system_clock::now(); // 計測開始時間
+    
+    // マルチスレッド設定
+    #pragma omp parallel for schedule(dynamic, 1)
+    for (int i = 0; i < width * height; ++i)
+    {
+        thread_local Random rng(42 + omp_get_thread_num()); // スレッドごとの乱数生成器
+
+        for (int j = 0; j < SamplesPerPixel; ++j)
+        {
+            const int x = i % width;
+            const int y = height - i / width; // 上下反転
+            Ray ray;
+            ray.o = camera.eye;
+            ray.d = [&]()
+            {
+                // プライマリレイの生成
+                // 1. カメラ座標系での方向を求める
+                // 2. それをワールド座標系に変換
+                const double tf = std::tan(camera.fov * 0.5);
+                // ピクセル内でランダムな位置を決める
+                const double rpx = 2.0 * (x + rng.Next()) / width - 1;
+                const double rpy = 2.0 * (y + rng.Next()) / height - 1;
+                const V w = normalize(V(camera.aspect * tf * rpx, tf * rpy, -1.0));
+                return uE * w.x + vE * w.y + wE * w.z;
+            }();
+
+            // レイの方向が決まったので、光の反射を計算していく
+            V L(0);  // 照度の一時変数
+            V th(1); // 光の経路を通した輝度の変化を記録するための変数（throughput）
+            for (int depth = 0; depth < param.depth; ++depth)
+            {
+                // 与えられたレイに対する交差判定
+                if (const auto h = scene.Intersect(ray, 1e-4, 1e+10)) // 自分自身にぶつかるのを避けるため、1e-4 のような小さい値を入れる
+                {
+                    // 反射回数ごとの輝度を計算して足し合わせる
+                    L = L + th * h->sphere->Le;
+                    // 交差した位置をもとにして次のレイを生成する
+                    ray.o = h->p;
+                    // レイの方向はランダムに計算する
+                    ray.d = [&]() {
+                        // ローカル座標の中での方向
+
+                        // 材質ごとの反射を計算
+                        switch (h->sphere->type)
+                        {
+                            // 拡散反射
+                            case SurfaceType::Diffuse:
+                            {
+                                // 1. 接空間の基底ベクトルを求める
+                                // 2. 1. の空間で次の方向をランダムに生成
+                                // 3. 2. の方向をワールド座標系に変換
+                                const auto n = dot(h->n, -ray.d) > 0 ? h->n : -h->n;
+                                const auto& tuple = tangentSpace(n);
+                                const auto d = [&]() {
+                                    const double r = sqrt(rng.Next());
+                                    const double t = 2.0 * M_PI * rng.Next();
+                                    const double x = r * cos(t);
+                                    const double y = r * sin(t);
+                                    return V(x, y,
+                                        std::sqrt(
+                                            std::max(.0, 1 - x*x - y*y)));
+                                }();
+                                // 方向のワールド座標変換
+                                return std::get<0>(tuple) * d.x + std::get<1>(tuple) * d.y + n * d.z;
+                            }
+                            // 鏡面反射
+                            case SurfaceType::Mirror:
+                            {
+                                const auto wi = -ray.d;
+                                return 2 * dot(wi, h->n) * h->n - wi;
+                            }
+                        }
+                        // ここには到達しないはず
+                        return V();
+                    }();
+                    // throughput を更新
+                    th = th * h->sphere->R;
+                    if (std::max({ th.x, th.y, th.z }) == 0)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            image[i] = image[i] + L / SamplesPerPixel;
+        }
+    }
+    const int64_t elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start).count();
+    std::cout << elapsed << " millisec." << std::endl;
+}
+
 int main()
 {
-    const int width = 200;
-    const int height = 200;
-    const int samplesPerPixel = 100;
+    const int width = 800;
+    const int height = 600;
+    const int samplesPerPixel = 1000;
     const int depth = 10;
 
     RenderParam param;
@@ -275,7 +406,7 @@ int main()
     param.depth = depth;
 
     std::vector<V> I(width * height);
-    chapter1(I, param);
+    chapter2(I, param);
 
     PPM ppm(width, height);
     for (int i = 0; i < width * height; ++i)
